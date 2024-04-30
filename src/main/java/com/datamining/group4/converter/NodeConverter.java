b1c0c7ad7c7f88f26514562884740028a2764e94
henrytran1604 <quanghuy1604nd@gmail.com>
2024-04-30 15:56:03 +0700

    update: remove list children

--- src/main/java/com/datamining/group4/converter/NodeConverter.java
+++ src/main/java/com/datamining/group4/converter/NodeConverter.java
@@ -9,19 +9,13 @@ import java.util.stream.Stream;
 
 @Component
 public class NodeConverter {
-    public Node toEntity(NodeDTO dto) {
-        Node entity = new Node();
-        entity.setItemName(dto.getName());
-        entity.setChildren(dto.getChildren().stream().map(this::toEntity).toList());
 
-        return entity;
-    }
     public NodeDTO toDto(Node entity) {
         NodeDTO dto = new NodeDTO();
         dto.setName(entity.getItemName());
         Attributes att = new Attributes(entity.getSupportCount());
         dto.setAttributes(att);
-        dto.setChildren(entity.getChildren().stream().map(this::toDto).toList());
+        dto.setChildren(entity.getMapChildren().values().stream().map(this::toDto).toList());
         return dto;
     }
 }
